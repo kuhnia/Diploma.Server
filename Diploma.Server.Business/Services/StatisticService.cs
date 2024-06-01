b7b1857647826fb6407cced11c3a0b30cc7b523f
mchepil <mchepil@mint-innovations.com>
2024-06-01 13:26:41 +0300

    Add project files.

---
+++ Diploma.Server.Business/Services/StatisticService.cs
@@ -0,0 +1,53 @@
+﻿using Diploma.Entities.Models;
+using Diploma.Server.Business.Interfaces;
+using Diploma.Server.DataAccess;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diploma.Server.Business.Services
+{
+    public class StatisticService : IStatisticService
+    {
+        private readonly ILogger<StatisticService> _logger;
+
+        private readonly StatisticDataController _statisticDataController;
+
+        public StatisticService(ILogger<StatisticService> logger, StatisticDataController statisticDataController)
+        {
+            _logger = logger;
+            _statisticDataController = statisticDataController;
+        }
+
+        public async Task<bool> CreateStatisticRecord(CounterSnapshot counterSnapshot)
+        {
+            try
+            {
+                var res = await _statisticDataController.CreateStatisticRecord(counterSnapshot);
+                return res;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return false;
+            }
+        }
+
+        public async Task<IEnumerable<CounterSnapshot>> GetAllStatistic()
+        {
+            try
+            {
+                var res = await _statisticDataController.GetAllStatistic();
+                return res;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ Diploma.Server.DataAccess/StatisticDataController.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using Diploma.Entities.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diploma.Server.DataAccess
+{
+    public class StatisticDataController : DataController
+    {
+        public StatisticDataController(IConfiguration configuration) : base(configuration, "IssueReports")
+        {
+        }
+
+        public Task<bool> CreateStatisticRecord(CounterSnapshot counterSnapshot)
+        {
+            if (counterSnapshot == null)
+                throw new ArgumentNullException(nameof(counterSnapshot));
+
+            var parameters = new DynamicParameters();
+            parameters.Add("@Id", counterSnapshot.Id);
+            parameters.Add("@CurrentValue", counterSnapshot.CurrentValue);
+            parameters.Add("@DifferenceWithPreviousValue", counterSnapshot.DifferenceWithPreviousValue);
+            return PerformNonQuery(parameters, "Create");
+        }
+
+        public Task<IEnumerable<CounterSnapshot>> GetAllStatistic()
+        {
+            return GetManyAsync<CounterSnapshot>("GetAll");
+        }
+    }
+}
